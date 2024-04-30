abc62ba4c1b5516ae8765863a020c149a7c39953
Gabriel Bueno <127902855+gabebuenu@users.noreply.github.com>
2024-04-30 00:22:03 -0300

    Update AboutView.vue

--- rickandmorty/src/views/AboutView.vue
+++ rickandmorty/src/views/AboutView.vue
@@ -4,7 +4,9 @@
     </div>
     <figure class="figure">
       <img src="https://avatars.githubusercontent.com/u/127902855?s=400&u=b5e4aa7d42a7a303346dce2927e1a75a7c84b54e&v=4" class="figure-img img-fluid rounded" alt="..." style="border-radius: 40px;">
-      <figcaption class="figure-caption"><b>Gabriel Bueno da Silva.</b><p>
+      <figcaption class="figure-caption"><b>Gabriel Bueno da Silva</b><p>
+        <b>RA: 1960373</b><br>
+        <b>Termo: 3° A</b><br>
         Aluno de Análise e Desenvolvimento de Sistemas, tenho 18 anos. Atualmente faço parte de um projeto da CAPES e bolsista.</p>
       </figcaption>
     </figure>
@@ -15,4 +17,3 @@
 </div>
 </section>
 </template>
-
